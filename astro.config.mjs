19b356a42ac25305099920ad73921d10e067d356
Dean Yeager <dean@studio10b.com>
2025-04-16 00:07:50 -0400

    Installed Amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import logoAnimation from './src/integrations/astro-logo-animation.js';
+//import netlify from '@astrojs/adapter-netlify';
 
 // https://astro.build/config
 export default defineConfig({
